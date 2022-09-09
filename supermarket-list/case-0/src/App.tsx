2eff700171527e0873e912729a4538bfd83c4565
José María <josemmazzei@gmail.com>
2022-09-08 23:18:03 -0300

    Resolve supermarket-list/case-0

--- supermarket-list/case-0/src/App.tsx
+++ supermarket-list/case-0/src/App.tsx
@@ -1,28 +1,37 @@
-import type {Item} from "./types";
+import type { Item } from "./types";
 
-import {useEffect, useState} from "react";
+import { useRef, useEffect, useState } from "react";
 
 import styles from "./App.module.scss";
 import api from "./api";
 
+//it also could be done with an autoFocus
 function App() {
   const [items, setItems] = useState<Item[] | null>(null);
 
+  const ref = useRef(null);
+
   useEffect(() => {
+    ref.current.focus();
     api.list().then(setItems);
   }, []);
+
+  const handleDelete = (id) => {
+    setItems((items) => items?.filter((item) => item.id != id));
+  };
 
   return (
     <main className={styles.main}>
       <h1>Supermarket list</h1>
       <form>
-        <input name="text" type="text" />
+        <input ref={ref} name="text" type="text" />
         <button>Add</button>
       </form>
       <ul>
-        {items.map((item) => (
-          <li className={item.completed ? styles.completed : ""}>
-            {item.text} <button>[X]</button>
+        {items?.map((item) => (
+          <li key={item.id} className={item.completed ? styles.completed : ""}>
+            {item.text}{" "}
+            <button onClick={() => handleDelete(item.id)}>[X]</button>
           </li>
         ))}
       </ul>
